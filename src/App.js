89ba9bff01a0d9e03a4e4612c06d98593b4606b2
Boris Civcic <civcicboris93@gmail.com>
2019-01-22 10:09:46 +0100

    redux added to the project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { simpleAction } from './store/actions/simpleAction'
 
 import Authen from './Authen'
 import './App.css';
 
 class App extends Component {
+  simpleAction = (event) => {
+    this.props.simpleAction();
+  }
   render() {
     return (
       <div className="App">
         <Authen />
+        <button onClick={this.simpleAction}>Test redux action</button>
+        <pre>
+          {
+            JSON.stringify(this.props)
+          }
+        </pre>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  ...state
+})
+
+const mapDispatchToProps = dispatch => ({
+  simpleAction: () => dispatch(simpleAction())
+})
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+
--- src/Authen.js
+++ src/Authen.js
@@ -1,16 +1,8 @@
 import React, { Component } from 'react'
+import { FirebaseConfig } from "./config/dev";
 
 var firebase = require('firebase')
-
-var config = {
-  apiKey: "AIzaSyCM2lqMfWg1NNtTf6caURCixXSf5sDuOsw",
-  authDomain: "usurvey-3e521.firebaseapp.com",
-  databaseURL: "https://usurvey-3e521.firebaseio.com",
-  projectId: "usurvey-3e521",
-  storageBucket: "usurvey-3e521.appspot.com",
-  messagingSenderId: "536079421504"
-};
-firebase.initializeApp(config);
+firebase.initializeApp(FirebaseConfig);
 
 class Authen extends Component {
 
